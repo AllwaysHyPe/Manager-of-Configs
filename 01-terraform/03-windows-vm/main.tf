ed1375710ba4aed2475ec5bed7c2972b1684a426
Hailey Phillips <hailey@allwayshype.com>
2026-05-05 00:26:11 -0500

    updating keyvault access policy

--- 01-terraform/03-windows-vm/main.tf
+++ 01-terraform/03-windows-vm/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_key_vault" "main" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = ["Get", "Set", "Delete", "Purge"]
+    secret_permissions = ["Get", "Set", "List", "Delete", "Purge"]
   }
 }
 
